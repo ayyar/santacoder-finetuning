7249362285eee971ee14acf89302d11ff81c53f0
Kartik Ayyar <ayyar@brownfoxlabs.com>
2023-04-15 16:53:09 -0700

    Cleanup.

--- train.py
+++ train.py
@@ -2,7 +2,6 @@
 Fine-Tune SantaCoder on code/text dataset
 """
 
-import argparse
 import os
 import random
 
@@ -20,26 +19,31 @@ from transformers import (
     logging,
     set_seed,
 )
+import pdb
+import datasets
 
 import fim
+
+import argparse
 
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model_path", type=str, default="bigcode/santacoder")
-    parser.add_argument("--dataset_name", type=str, default="bigcode/the-stack-dedup")
-    parser.add_argument("--subset", type=str, default="data")
-    parser.add_argument("--split", type=str, default="train")
+    # parser.add_argument("--model_path", type=str, default="Roblox/bigcode15b")
+    parser.add_argument(
+        "--model_path", type=str, default="arjunguha/santacoder-lua-nofim"
+    )
+    parser.add_argument("--subset", type=str, default="train_data")
     parser.add_argument("--size_valid_set", type=int, default=4000)
     parser.add_argument("--streaming", action="store_true")
     parser.add_argument("--shuffle_buffer", type=int, default=5000)
     parser.add_argument("--data_column", type=str, default="content")
 
     parser.add_argument("--seq_length", type=int, default=1024)
-    parser.add_argument("--max_steps", type=int, default=10000)
+    parser.add_argument("--max_steps", type=int, default=100)
     parser.add_argument("--batch_size", type=int, default=2)
     parser.add_argument("--gradient_accumulation_steps", type=int, default=8)
-    parser.add_argument("--eos_token_id", type=int, default=49152)
+    parser.add_argument("--eos_token_id", type=int, default=35262)
 
     parser.add_argument("--learning_rate", type=float, default=5e-5)
     parser.add_argument("--lr_scheduler_type", type=str, default="cosine")
@@ -54,7 +58,7 @@ def get_args():
     parser.add_argument("--num_workers", type=int, default=None)
     parser.add_argument("--output_dir", type=str, default="./checkpoints")
     parser.add_argument("--log_freq", default=1, type=int)
-    parser.add_argument("--eval_freq", default=1000, type=int)
+    parser.add_argument("--eval_freq", default=10, type=int)
     parser.add_argument("--save_freq", default=1000, type=int)
 
     parser.add_argument("--fim_rate", type=float, default=0)
@@ -172,26 +176,38 @@ class ConstantLengthDataset(IterableDataset):
             for example in examples:
                 self.current_size += 1
                 yield {
-                        "input_ids": torch.LongTensor(example),
-                        "labels": torch.LongTensor(example),
-                    }
+                    "input_ids": torch.LongTensor(example),
+                    "labels": torch.LongTensor(example),
+                }
+
 
 def create_datasets(tokenizer, args):
-    dataset = load_dataset(
-        args.dataset_name,
-        data_dir=args.subset,
-        split=args.split,
+    data_files = {"train": "train.jsonl", "test": "test.jsonl"}
+    load_args = dict(
+        data_dir="train_data",
+        data_files=data_files,
         use_auth_token=True,
         num_proc=args.num_workers if not args.streaming else None,
         streaming=args.streaming,
     )
+
+    dataset = load_dataset(
+        "json",
+        **load_args,
+    )
+
+    def add_content(example):
+        example[args.data_column] = example["prompt"] + example["completion"]
+        return example
+
+    dataset = dataset.map(add_content)
+
     if args.streaming:
         print("Loading the dataset in streaming mode")
         valid_data = dataset.take(args.size_valid_set)
         train_data = dataset.skip(args.size_valid_set)
         train_data = train_data.shuffle(buffer_size=args.shuffle_buffer, seed=args.seed)
     else:
-        dataset = dataset.train_test_split(test_size=0.005, seed=args.seed)
         train_data = dataset["train"]
         valid_data = dataset["test"]
         print(
@@ -271,8 +287,7 @@ def run_training(args, train_data, val_data):
 
 
 def main(args):
-    tokenizer = AutoTokenizer.from_pretrained(args.model_path, use_auth_token=True)
-
+    tokenizer = AutoTokenizer.from_pretrained(args.tokenizer_id, use_auth_token=True)
     train_dataset, eval_dataset = create_datasets(tokenizer, args)
 
     run_training(args, train_dataset, eval_dataset)
